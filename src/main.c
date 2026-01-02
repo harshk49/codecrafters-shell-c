e6d35ae78d693ed82befd79835be9610922c74f3
Harsh Kardile <harshkardile49@gmail.com>
2026-01-03 02:17:46 +0530

    refactor: reorganize input handling and error message printing

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+  // Read User's Input
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+
+  // Remove newline character from input
+  command[strcspn(command, "\n")] = "\0";
+
+  //Print error Message
+  printf("%s: command not found\n", command);
 
   return 0;
 }
